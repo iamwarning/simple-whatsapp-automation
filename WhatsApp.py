ad8e459f81c86daa258b3eb396956aa620fcca79
baos <siul.gomez94@gmail.com>
2020-05-09 22:05:39 -0500

    Whatsapp Automation Script

---
+++ WhatsApp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import  Keys
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Chrome("/home/baos/Documents/baos/Python/whatsapp/WhatsApp Automation/./chromedriver")
+
+browser.get("https://web.whatsapp.com/")
+wait = WebDriverWait(browser, 600)
+
+target = '" _user_ "'
+string = " _message_ "
+x_arg = '//span[contains(@title, ' + target + ')]'
+target = wait.until(ec.presence_of_element_located((By.XPATH, x_arg)))
+target.click()
+
+input_box = browser.find_element_by_class_name("_1Plpp")
+for i in range(100):
+    input_box.send_keys(string + Keys.ENTER)
+
